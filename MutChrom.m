92be1e5172dd6bce87e6a4801e95a337967185c7
JIKUN1990 <72196707+JIKUN1990@users.noreply.github.com>
2020-10-03 23:00:20 +0800

    Update MutChrom.m

--- MutChrom.m
+++ MutChrom.m
@@ -1,13 +1,11 @@
 function [chrom,location] = MutChrom(chrom, mut, N, N_chrom,location,A)
-% 去除和chrom相同的数字
-
 
 for i = 1:N
    s=N-N_chrom;
 
     for j = 1:N_chrom
-        mut_rand = rand; %是否变异
-        if mut_rand <=mut %%变异概率mut
+        mut_rand = rand; 
+        if mut_rand <=mut 
         ss=1:N;
         ss(location(i,:))=[];
            b=randi([1 s]);
